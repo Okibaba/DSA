241c5ea405979bb03cd23bdd675013968bb226fc
Okibaba <abiodunoki@gmail.com>
2024-12-17 07:55:08 -0700

    binary tree

---
+++ DFS/binary_tree_path_sum.py
@@ -0,0 +1,7 @@
+"""
+Problem Statement
+Given a root of the binary tree and an integer ‘S’, 
+return true if the tree has a path from root-to-leaf 
+such that the sum of all the node values of that path 
+equals ‘S’. Otherwise, return false.
+"""
