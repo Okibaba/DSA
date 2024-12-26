558ce027159ad40f1ea4e9f8a17eac391b833447
Okibaba <abiodunoki@gmail.com>
2024-12-26 13:10:45 -0700

    backtracking

--- DFS/binary_tree_path_sum.py
+++ DFS/binary_tree_path_sum.py
@@ -5,3 +5,30 @@ return true if the tree has a path from root-to-leaf
 such that the sum of all the node values of that path 
 equals ‘S’. Otherwise, return false.
 """
+
+
+class TreeNode(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+class Solution:
+    def hasPath(self, sum, root):
+        if root is None:
+            return False
+
+        if sum == root.value and root.left is None and root.right is None:
+            return True
+
+        return self.hasPath(root.left, sum-root.val) or self.hasPath(root.right, sum-root.val)
+
+
+class main():
+    root = Treenode(5)
+    root.left =
+    root.right =
+    root.left.left =
+    root.left.right =
+    root.right.left =
+    root.right.right =
---
+++ backtracking/mxn_grid_of_characters.py
@@ -0,0 +1,6 @@
+class Solution:
+    def dfs(self,board,word,i,j,k):
+    #check if current coordinates are out of grid or the current cell doesn't match the current character of the work
+    if board[i][j] !=word[k] or if not (0<=i<len(board))
+    
+   
