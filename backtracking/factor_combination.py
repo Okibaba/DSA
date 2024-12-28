e3a47e5d08b5cd4101b8cb6800d57f5180531b92
Okibaba <abiodunoki@gmail.com>
2024-12-28 10:38:32 -0700

    backtracking contd

---
+++ backtracking/factor_combination.py
@@ -0,0 +1,40 @@
+class Solution:
+    def getFactors(self,n):
+    
+       res=[]
+       self.backtrack_factor_check (n,comb,res)
+       
+       return res
+       
+       
+    def backtrack_factor_check (n,comb,res)
+       #exit conditions 
+       if n%num!=0 or if num>=n:
+        return False
+       
+       #success conditions
+       if n%product==0:
+        return True       
+         
+       
+       
+       
+       for num in range(n):
+           
+           #append some how
+           #if not back track
+           
+        
+                   
+        
+    
+       
+       #recursive function
+       
+       #pseudocode:
+       #write a backtracking recursive function that checks for products
+       #sequentially builds up from modulo
+       #append success items
+       #take into consideration lists
+       
+
